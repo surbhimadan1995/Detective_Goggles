3b998d15c6de3cffec47bd48e406e3182eee182c
Surbhi Madan <surbhi_madan@brown.edu>
2014-12-03 22:16:59 -0500

    Webpage layout css changes

--- obstacle_course.js
+++ obstacle_course.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
 	$("#goggles").draggable();
 	$("#paragraph_box" ).droppable({
       	drop: function( event, ui ) {
-      		//$(this).css({'background-color':'blue'});
+      		$(this).css({'background-color':'blue'});
+      		
+  			var script=document.createElement('script');
+      		script.src='http://localhost:8080/webxray.js';
+      		script.className='webxray';
+      		document.body.appendChild(script);
+      		
 
      	}
     });
